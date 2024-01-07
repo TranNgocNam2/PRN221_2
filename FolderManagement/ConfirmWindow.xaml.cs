fed6755900ff72639d59c33392d8c3b14642332e
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2024-01-07 18:30:23 +0700

    first commit

---
+++ FolderManagement/ConfirmWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FolderManagement
+{
+    /// <summary>
+    /// Interaction logic for ConfirmWindow.xaml
+    /// </summary>
+    public partial class ConfirmWindow : Window
+    {
+        public bool IsCreatingFile { get; private set; }
+        public string Path { get; private set; }
+        public List<MainWindow.Item> ItemList { get; private set; }
+        public ConfirmWindow(List<MainWindow.Item> itemList, string path)
+        {
+            InitializeComponent();
+            ItemList = itemList;
+            Path = path;
+        }
+
+        private void file_btn_Click(object sender, RoutedEventArgs e)
+        {
+            file_btn.Visibility = Visibility.Hidden;
+            file_name_label.Visibility = Visibility.Visible;
+            file_name_txt.Visibility = Visibility.Visible;
+            ok_btn.Visibility = Visibility.Visible;
+            folder_btn.Visibility= Visibility.Hidden;
+            IsCreatingFile = true;
+        }
+
+        private void ok_btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (IsCreatingFile)
+            {
+                case true:
+                    createFile(file_name_txt.Text, Path);
+                    break;
+                case false:
+                    createFolder(folder_name_txt.Text, Path);
+                    break;
+            }
+        }
+
+        private void folder_btn_Click(object sender, RoutedEventArgs e)
+        {
+            folder_btn.Visibility = Visibility.Hidden;
+            folder_name_label.Visibility = Visibility.Visible;
+            folder_name_txt.Visibility = Visibility.Visible;
+            ok_btn.Visibility = Visibility.Visible;
+            file_btn.Visibility = Visibility.Hidden;
+            IsCreatingFile = false;
+        }
+
+        private void createFile(string filename, string path)
+        {
+            if (!filename.Contains("."))
+            {
+                filename += ".txt";
+            }
+            try
+            {
+                string filePath = System.IO.Path.Combine(path, filename);
+                if (File.Exists(filePath)){
+                    MessageBox.Show("Existed file! Please try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                File.Create(filePath);
+                MessageBox.Show("Create new file successfully!", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+                ItemList.Add(new MainWindow.Item
+                {
+                    Type = "D:\\Code_PRN221\\FolderManagement\\FolderManagement\\Images\\file_icon.png",
+                    Name = filename,
+                    Path = filePath
+                });
+                var mainWindow = Application.Current.MainWindow as MainWindow;
+                mainWindow?.scanFilesAndSubfolders(Path);
+                Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+        }
+
+        private void createFolder(string foldername, string path)
+        {
+            try
+            {
+                string folderPath = System.IO.Path.Combine(path, foldername);
+                if (Directory.Exists(folderPath))
+                {
+                    MessageBox.Show("Existed folder! Please try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                Directory.CreateDirectory(folderPath);
+                MessageBox.Show("Create new folder successfully!", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+                ItemList.Add(new MainWindow.Item
+                {
+                    Type = "D:\\Code_PRN221\\FolderManagement\\FolderManagement\\Images\\folder_icon.png",
+                    Name = foldername,
+                    Path = folderPath
+                });
+                var mainWindow = Application.Current.MainWindow as MainWindow;
+                mainWindow?.scanFilesAndSubfolders(Path);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+        private void cancel_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ FolderManagement/MainWindow.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WinForm = System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.ObjectModel;
+using Path = System.IO.Path;
+
+namespace FolderManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            filesListView.SelectionChanged += filesListView_SelectionChanged;
+            filesListView.MouseDoubleClick += filesListView_MouseDoubleClick;
+        }
+        private string rootFolderPath = "";
+        public class Item
+        {
+            public string? Type { get; set; }
+            public string? Name { get; set; }
+            public string? Path { get; set; }
+        }
+        private void browse_file_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                WinForm.FolderBrowserDialog dialog = new WinForm.FolderBrowserDialog();
+                dialog.InitialDirectory = "";
+                WinForm.DialogResult result = dialog.ShowDialog();
+                if (result == WinForm.DialogResult.OK)
+                {
+                    rootFolderPath = dialog.SelectedPath;
+                    folder_path.Text = rootFolderPath;
+                    scanFilesAndSubfolders(rootFolderPath);
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        public void scanFilesAndSubfolders(string path)
+        {
+
+            var list = new List<Item>();
+            list.Clear();
+            try
+            {
+                var files = Directory.GetFiles(path);
+                var folders = Directory.GetDirectories(path);
+                
+                foreach (var file in files)
+                {
+                    list.Add(new Item 
+                    { 
+                        Type = "D:\\Code_PRN221\\FolderManagement\\FolderManagement\\Images\\file_icon.png", 
+                        Name = Path.GetFileName(file), 
+                        Path = Path.GetFullPath(file)
+                    });
+                } 
+
+                foreach (var folder in folders)
+                {
+                    list.Add(new Item 
+                    { 
+                        Type = "D:\\Code_PRN221\\FolderManagement\\FolderManagement\\Images\\folder_icon.png", 
+                        Name = Path.GetFileName(folder), 
+                        Path = Path.GetFullPath(folder) 
+                    });
+                }
+
+                // Bind the items to the ListView
+                filesListView.ItemsSource = list;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error displaying files: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+
+        private void create_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(rootFolderPath == null || rootFolderPath.Length == 0)
+            {
+                MessageBox.Show("Folder name cannot empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var confirmWindow = new ConfirmWindow((List<Item>) filesListView.ItemsSource, rootFolderPath);
+            confirmWindow.ShowDialog();
+            folder_path.Text = rootFolderPath;
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(folder_path.Text))
+            {
+                string pathToDelete = folder_path.Text;
+                if (File.Exists(pathToDelete))
+                {
+                    MessageBoxResult result = MessageBox.Show($"Do you want to delete file: {Path.GetFileName(pathToDelete)}?", "Notification", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                    deleteFile(pathToDelete);
+                    return;
+                }else if (Directory.Exists(pathToDelete)){
+                    MessageBoxResult result = MessageBox.Show($"Do you want to delete folder: {Path.GetFileName(pathToDelete)}?", "Notification", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                    deleteFolder(pathToDelete); 
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("The specified path does not exist!", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a file or folder to delete!", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void deleteFile(string filePath)
+        {
+            try
+            {
+                File.Delete(filePath);
+                MessageBox.Show($"File '{Path.GetFileName(filePath)}' deleted successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                folder_path.Text = rootFolderPath;
+                scanFilesAndSubfolders(rootFolderPath);
+            }
+            catch(Exception ex) {
+                MessageBox.Show($"Error deleting file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void deleteFolder(string folderPath)
+        {
+            try
+            {
+                Directory.Delete(folderPath, true);
+                MessageBox.Show($"Folder '{Path.GetFileName(folderPath)}' deleted successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                folder_path.Text = rootFolderPath;
+                scanFilesAndSubfolders(rootFolderPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting folder: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void filesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(filesListView.SelectedItems != null && filesListView.SelectedItems.Count > 0)
+            {
+                MainWindow.Item selectedFile = (MainWindow.Item)filesListView.SelectedItem;
+                if (selectedFile != null && !string.IsNullOrEmpty(selectedFile.Path))
+                {
+                    folder_path.Text = selectedFile.Path;
+                }
+            }
+        }
+
+        private void filesListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (filesListView.SelectedItems != null && filesListView.SelectedItems.Count > 0)
+            {
+                MainWindow.Item selectedItem = (MainWindow.Item)filesListView.SelectedItem;
+                if (selectedItem != null && !string.IsNullOrEmpty(selectedItem.Path))
+                {
+                    if (Directory.Exists(selectedItem.Path)) {
+                        rootFolderPath = selectedItem.Path;
+                        scanFilesAndSubfolders(selectedItem.Path);
+                    }
+                }
+            }
+        }
+
+        private void previous_folder_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(rootFolderPath))
+            {
+                DirectoryInfo currentDirectory = new DirectoryInfo(rootFolderPath);
+                if (currentDirectory.Parent != null)
+                {
+                    rootFolderPath = currentDirectory.Parent.FullName;
+                    folder_path.Text = rootFolderPath;
+                    scanFilesAndSubfolders(rootFolderPath);
+                }
+            }
+        }
+    }     
+}
